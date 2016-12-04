46941e3379129d59f906abfead17b01cd76fdd62
planet581g <planet581g@gmail.com>
2016-12-04 14:09:36 -0800

    Display 2D point cloud with binary classification.

--- source/news_popularity.py
+++ source/news_popularity.py
@@ -13,6 +13,7 @@ import numpy as np
 import plotly
 import plotly.graph_objs as go
 
+plot3D = 0
 
 # Import the features from the raw data file.
 X, T, N = fo.load_feature_matrix()
@@ -26,22 +27,28 @@ print("The max diff is ", np.amax(np.abs(diff)))
 
 # Calculate cumalitive variance and plot it.
 cum_variance = pca.create_var_plot(λ)
+plt.figure(1)
 plt.plot(cum_variance)
 
-# Create a 2D plot
-#plt.plot(P[:, 0:1], P[:, 1:2], 'r.')
 
-# Create a 3D plot
-#ax = fig.add_subplot(111, projection='3d')
-#ax.scatter(P[:, 0], P[:, 1], T)
+threshold = 1400  # Number of shares that indicates popular.
+
+# Get the popular records of the first and second principal components.
+P_pop = np.array([j for (i, j) in zip(T, P[:, 0:2]) if i >= 1400])  # popular records
+P_unpop = np.array([j for (i, j) in zip(T, P[:, 0:2]) if i < 1400])  # unpopular records
+
+# Create a 2D point cloud.
+plt.figure(2)
+plt.plot(P_pop[:, 0:1], P_pop[:, 1:2], 'r.')
+plt.plot(P_unpop[:, 0:1], P_unpop[:, 1:2], 'b.')
 
 # Create 3D plot with Plotly.
-trace1 = go.Scatter3d(x=P[:, 0], y=P[:, 1], z=T,
-                      mode='markers',
-                      marker=dict(size=4))
-#                      line=dict(color='rgba(217, 217, 217, 0.14)', width=0.1),
-#                      opacity=0.9)
-layout = go.Layout(margin=dict(l=0, r=0, b=0, t=0))
-
-fig = go.Figure(data=[trace1], layout=layout)
-plotly.offline.plot(fig, filename='simple-3d-scatter')
+if plot3D == 1:
+    trace1 = go.Scatter3d(x=P[:, 0], y=P[:, 1], z=T,
+                          mode='markers',
+                          marker=dict(size=4))
+    #                      line=dict(color='rgba(217, 217, 217, 0.14)', width=0.1),
+    #                      opacity=0.9)
+    layout = go.Layout(margin=dict(l=0, r=0, b=0, t=0))
+    fig = go.Figure(data=[trace1], layout=layout)
+    plotly.offline.plot(fig, filename='simple-3d-scatter')
