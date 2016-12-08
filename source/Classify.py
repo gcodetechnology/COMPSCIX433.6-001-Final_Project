da9d4ee22560d30a3cb710641df0fb7451db5ca3
pollyyu <paulynnyu@gmail.com>
2016-12-07 19:25:47 -0800

    Add files via upload

--- source/Classify.py
+++ source/Classify.py
@@ -7,10 +7,14 @@ from numpy import *
 import scipy.sparse as sparse
 import scipy.linalg as linalg
 
+###############################
+### download & explore data ###
+###############################
 
 csvfile = open('OnlineNewsPopularity.csv')
 data = pd.read_csv(csvfile, header = 0)
 
+#just getting general information on 'shares'
 target = data[' shares']
 print(target.describe())
 #count     39644.000000
@@ -22,14 +26,19 @@ print(target.describe())
 #75%        2800.000000
 #max      843300.000000
 
-# we can see that there are huge outliers (max = 843300 vs mode of 1400)
+    # we can see that there are huge outliers (max = 843300 vs mode of 1400)
 
-# plt.hist(target, bins = 400)
-# plt.show
+    # plt.hist(target, bins = 400)
+    # plt.show
 
-# determining popular and viral videos
-popular = np.percentile(target, 95) # 10800 shares
-viral = np.percentile(target, 99.5) # 50679 shares
+############################################
+### determining popular and viral vidoes ###
+############################################
+target = data[' shares']
+popular = np.percentile(target, 50)
+viral = np.percentile(target, 50.005)
+print(popular)
+print(viral)
 
 #creating classification
 data['normal'] = -1
@@ -44,26 +53,53 @@ for article in range (len(data)):
     elif data.loc[article, ' shares'] >= viral:
         data.loc[article, 'viral'] += 2
 
-# create augmented X
-X = data.iloc[:,2:59]
-a = np.ones((len(data),1))
-Xa = np.hstack((a,X))
-Xap = np.linalg.pinv(Xa)
-print(Xap.shape)
-# create target
-target = data[['normal','popular','viral']]
-W = np.dot(Xap, target)
+# create training and test data
+msk = np.random.rand(len(data)) < 0.75
+train = data[msk]
+test = data[~msk]
 
-# model assessment
-# T_p = np.dot(Xap, W)
-# T_c = np.zeros((len(T_p),))
-# for i in range(0,len(T_p[:,1])):
-#     T_c[i] = np.argmax(T_p[i,:])
-#
-# comparisons = np.zeros((3, 3))
-# for i in range(0, len(T_c)):
-#     actual = target[i]
-#     predicted = T_c[i] # if you don't use the 0 at the end, this is an array
-#     comparisons[actual, predicted] += 1
-#
-# print(comparisons)
+######################
+### building model ###
+######################
+
+# create augmented X
+X_train = train.iloc[:,2:60]
+a = np.ones((len(train),1))
+Xa_train = np.hstack((a,X_train))
+# find pseudoinverse
+Xap_train = np.linalg.pinv(Xa_train)
+# create target
+target_train = train[['normal','popular','viral']]
+target_train = np.asarray(target_train)
+# finding W
+W = np.dot(Xap_train, target_train)
+
+########################
+### model assessment ###
+########################
+# building Xa for test
+X_test = test.iloc[:,2:60]
+a = np.ones((len(test),1))
+Xa_test = np.hstack((a,X_test))
+# making classifier
+T_p = np.dot(Xa_test, W)
+T_classify = np.zeros((len(T_p),))
+for i in range(0,len(T_p[:,1])):
+    T_classify[i] = np.argmax(T_p[i,:])
+
+# create target
+target_test = test[['normal','popular','viral']]
+target_test = np.asarray(target_test)
+# convert target into 0,1,2 within one column
+T_data = np.zeros((len(T_p),))
+for i in range(0,len(T_p[:,1])):
+    T_data[i] = np.argmax(target_test[i,:])
+
+# making comparisons
+comparisons = np.zeros((3, 3))
+for i in range(0, len(T_p)):
+    actual = T_data[i]
+    predicted = T_classify[i] # if you don't use the 0 at the end, this is an array
+    comparisons[actual, predicted] += 1
+
+print(comparisons)
